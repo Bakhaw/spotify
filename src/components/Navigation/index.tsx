2459ffd647bcf623c7c042583b2233e894949c4f
Hattou Faïssal <faissal.hattou@gmail.com>
2023-04-17 00:55:23 +0200

    Navigation mobile first

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,63 +1,54 @@
-import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import {
   HomeIcon,
   MagnifyingGlassIcon,
   MusicalNoteIcon,
+  UserIcon,
 } from "@heroicons/react/24/outline";
-
-import useTrack from "@/hooks/useTrack";
-import SignOut from "../SignOut";
-import Playlists from "../Playlists";
 
 function Navigation() {
   const router = useRouter();
-  const track = useTrack();
 
   if (router.asPath === "/login") return null;
 
   const links = [
     {
       href: "/",
-      icon: <HomeIcon className="h-6 w-6" />,
+      icon: <HomeIcon className="h-5 w-5" />,
       label: "Home",
     },
     {
       href: "/search",
-      icon: <MagnifyingGlassIcon className="h-6 w-6" />,
+      icon: <MagnifyingGlassIcon className="h-5 w-5" />,
       label: "Search",
     },
     {
       href: "/library",
-      icon: <MusicalNoteIcon className="h-6 w-6" />,
+      icon: <MusicalNoteIcon className="h-5 w-5" />,
       label: "Library",
+    },
+    {
+      href: "/account",
+      icon: <UserIcon className="h-5 w-5" />,
+      label: "Account",
     },
   ];
 
   return (
-    <nav
-      className="hidden md:flex flex-col justify-between items-start gap-4 w-60 bg-[#2d2e37] p-4 overflow-x-hidden overflow-y-scroll scrollbar-hide"
-      style={{
-        height: track ? "calc(100vh - 80px)" : "100vh",
-      }}
-    >
-      <ul className="flex flex-col gap-4">
+    <nav className="w-full bg-[#2d2e37] p-4 overflow-x-hidden overflow-y-scroll scrollbar-hide">
+      <ul className="flex justify-around items-center">
         {links.map((link, index) => (
           <Link
             key={index}
-            className="flex items-center justify-start gap-1"
+            className="flex flex-col justify-center items-center gap-1"
             href={link.href}
           >
             {link.icon}
-            <span>{link.label}</span>
+            <h1 className="text-sm">{link.label}</h1>
           </Link>
         ))}
       </ul>
-
-      <Playlists />
-
-      <SignOut />
     </nav>
   );
 }
