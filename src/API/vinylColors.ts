4acf909e22ec1a228015fa637e4a6028720ef9a9
Hattou Faïssal <faissal.hattou@gmail.com>
2023-04-20 04:09:13 +0200

    remove unused file

--- src/API/vinylColors.ts
+++ src/API/vinylColors.ts
@@ -1,11 +1,8 @@
-import fullVinylColors from "./fullVinylColors";
-
 const vinylColors = [
   {
     albumId: "",
     backgroundColor: "",
   },
-  ...fullVinylColors,
 ];
 
 export default vinylColors;
