707b3e61b32bed225a3faa487d79ec8495b5149a
Hattou Faïssal <faissal.hattou@gmail.com>
2023-04-25 23:35:07 +0200

    add pull to refresh (may remove it if it's bad)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
+import { PullToRefresh } from "react-js-pull-to-refresh";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { RecoilRoot } from "recoil";
 
 import Container from "@/components/Container";
@@ -10,17 +12,33 @@ import "@/styles/globals.css";
 import "@/styles/vinyl.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  async function onRefresh() {
+    router.reload();
+  }
+
   return (
     <SessionProvider session={pageProps.session}>
       <RecoilRoot>
-        <div className="relative flex flex-col justify-between h-screen">
-          <Container>
-            <Component {...pageProps} />
-          </Container>
+        <PullToRefresh
+          pullDownContent={<div className="text-center">❤</div>}
+          releaseContent={<div />}
+          refreshContent={<div />}
+          pullDownThreshold={200}
+          onRefresh={onRefresh}
+          triggerHeight={50}
+          startInvisible={true}
+        >
+          <div className="relative flex flex-col justify-between h-screen">
+            <Container>
+              <Component {...pageProps} />
+            </Container>
 
-          <Player />
-          <Navigation />
-        </div>
+            <Player />
+            <Navigation />
+          </div>
+        </PullToRefresh>
       </RecoilRoot>
     </SessionProvider>
   );
