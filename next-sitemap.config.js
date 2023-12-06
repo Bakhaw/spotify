7cb9d555e87abe625ca3ead755a6721c6cfda815
Hattou Faïssal <faissal.hattou@gmail.com>
2023-12-06 01:21:57 +0100

    trying next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
+
+  additionalPaths: async (config) => {
+    const result = [];
+
+    // required value only
+    result.push({ loc: "/login" });
+    return result;
+  },
 };
