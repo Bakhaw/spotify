74730bb29c632ea5c6e1147876d6cf5354bb6499
Hattou Faïssal <faissal.hattou@gmail.com>
2023-12-07 06:25:41 +0100

    refactor: add react-icons (removing lucide-react)

--- src/components/Player/ClosedPlayer.tsx
+++ src/components/Player/ClosedPlayer.tsx
@@ -1,13 +1,8 @@
 import { useRecoilValue } from "recoil";
-import {
-  ChevronUpIcon,
-  PauseIcon,
-  PlayIcon,
-  SkipBackIcon,
-  SkipForwardIcon,
-  Volume1Icon,
-  Volume2Icon,
-} from "lucide-react";
+import { ChevronUpIcon, Volume1Icon, Volume2Icon } from "lucide-react";
+
+import { FaCirclePlay, FaCirclePause } from "react-icons/fa6";
+import { IoPlaySkipBack, IoPlaySkipForward } from "react-icons/io5";
 
 import { isPlayingState } from "@/atoms/trackAtom";
 
@@ -56,18 +51,26 @@ const ClosedPlayer: React.FC<ClosedPlayerProps> = ({
         onClick={onOpen}
       />
 
-      <div className="hidden md:flex flex-1 justify-center items-center gap-3 h-full">
-        <SkipBackIcon
+      <div className="hidden md:flex flex-1 justify-center items-center gap-4 h-full">
+        <IoPlaySkipBack
           className="h-6 w-6"
           role="button"
           onClick={onBackwardButtonClick}
         />
         {isPlaying ? (
-          <PauseIcon className="h-8 w-8" role="button" onClick={onTogglePlay} />
+          <FaCirclePause
+            className="h-9 w-9"
+            onClick={onTogglePlay}
+            role="button"
+          />
         ) : (
-          <PlayIcon className="h-8 w-8" role="button" onClick={onTogglePlay} />
+          <FaCirclePlay
+            className="h-9 w-9"
+            onClick={onTogglePlay}
+            role="button"
+          />
         )}
-        <SkipForwardIcon
+        <IoPlaySkipForward
           className="h-6 w-6"
           role="button"
           onClick={onForwardButtonClick}
--- src/lib/routes.tsx
+++ src/lib/routes.tsx
@@ -6,32 +6,31 @@ import {
   User2Icon,
 } from "lucide-react";
 
+import { LuLibrary, LuMusic2 } from "react-icons/lu";
+import { HiOutlineSquares2X2 } from "react-icons/hi2";
+import { PiVinylRecord } from "react-icons/pi";
+
 const routes = [
   {
     href: "/",
-    icon: <MusicIcon className="h-5 w-5" />,
+    icon: <LuMusic2 className="h-4 w-4" />,
     text: "listen now",
   },
   {
     disabled: true,
     href: "/search",
-    icon: <SearchIcon className="h-5 w-5" />,
+    icon: <HiOutlineSquares2X2 className="h-4 w-4" />,
     text: "browse",
   },
   {
     href: "/library",
-    icon: <LibraryIcon className="h-5 w-5" />,
+    icon: <LuLibrary className="h-5 w-5" />,
     text: "library",
   },
   {
     href: "/studio",
-    icon: <SpeakerIcon className="h-5 w-5" />,
+    icon: <PiVinylRecord className="h-5 w-5" />,
     text: "studio (beta)",
-  },
-  {
-    href: "/profile",
-    icon: <User2Icon className="h-5 w-5" />,
-    text: "profile",
   },
 ];
 
