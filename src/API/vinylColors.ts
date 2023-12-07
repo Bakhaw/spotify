338763a0a3fec8a8d07050a9bd0c380212972cb2
Hattou Faïssal <faissal.hattou@gmail.com>
2023-12-07 03:51:00 +0100

    fix: remove conditional rule on vinylColors

--- src/API/vinylColors.ts
+++ src/API/vinylColors.ts
@@ -1,11 +1,8 @@
-import fullVinylColors from "./fullVinylColors";
-
 const vinylColors = [
   {
     albumId: "",
     backgroundColor: "",
   },
-  ...(fullVinylColors && [...fullVinylColors]),
 ];
 
 export default vinylColors;
