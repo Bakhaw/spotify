65af1219d5d878b612d9473c9f28fb53c58012df
Hattou Faïssal <faissal.hattou@gmail.com>
2023-12-07 04:26:40 +0100

    fix: declare module colorthief

--- colorthief.d.ts
+++ colorthief.d.ts
@@ -2,6 +2,5 @@ declare module "colorthief" {
   type Color = [number, number, number];
   export default class ColorThief {
     getColor: (img: HTMLImageElement | null) => Color;
-    getPalette: (img: HTMLImageElement | null) => Color[];
   }
 }
