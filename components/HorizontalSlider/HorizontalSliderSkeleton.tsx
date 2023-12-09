f44d17dde3ace74a6c1150efdc6f07fa0c8f812b
Hattou Faïssal <faissal.hattou@gmail.com>
2023-12-09 16:17:12 +0100

    style: fix skeleton width and remove animate-pulse

--- components/HorizontalSlider/HorizontalSliderSkeleton.tsx
+++ components/HorizontalSlider/HorizontalSliderSkeleton.tsx
@@ -4,7 +4,7 @@ function HorizontalSliderSkeleton() {
   const mockItems = [0, 1, 2, 3, 4, 5, 6, 7];
 
   return (
-    <ul className="flex gap-5 px-8">
+    <ul className="flex gap-5 px-8 overflow-hidden">
       {mockItems.map((d) => (
         <div key={d} className="flex flex-col gap-3">
           <Skeleton className="h-[200px] w-[200px]" />
