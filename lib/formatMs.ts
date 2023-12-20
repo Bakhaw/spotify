a40fb03d9c9d97272622e583f9dc3931956805ef
Hattou Faïssal <faissal.hattou@gmail.com>
2023-12-20 18:07:09 +0100

    :recycle: formatMs now has a correct format

--- lib/formatMs.ts
+++ lib/formatMs.ts
@@ -1,6 +1,6 @@
 function formatMs(ms: number, outputFormat: "default" | "clock" = "default") {
   const minutes = Math.floor(ms / 60000);
-  const seconds = ((ms % 60000) / 1000).toFixed(0);
+  const seconds = ((ms % 60000) / 1000).toFixed(0).padStart(2, "0");
 
   const baseFormat = `${minutes}min ${seconds}s`;
   const clockFormat = `${minutes}:${seconds}`;
