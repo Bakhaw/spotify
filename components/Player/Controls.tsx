105628e5bd92c71c62462dc157aeb51dad931acb
Hattou Faïssal <faissal.hattou@gmail.com>
2023-12-21 00:00:50 +0100

    :lipstick: improved play/pause icons

--- components/Player/Controls.tsx
+++ components/Player/Controls.tsx
@@ -51,17 +51,35 @@ const Controls = () => {
         onClick={onBackwardButtonClick}
       />
       {currentPlaybackState?.is_playing ? (
-        <FaCirclePause
-          className="h-9 w-9 transition-all	hover:scale-110"
+        <div
+          className="h-full p-2 rounded-full bg-white cursor-pointer transition-all hover:scale-105"
           onClick={onTogglePlay}
-          role="button"
-        />
+        >
+          <svg
+            data-encore-id="icon"
+            role="button"
+            aria-hidden="true"
+            viewBox="0 0 16 16"
+            className="h-4 w-4"
+          >
+            <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
+          </svg>
+        </div>
       ) : (
-        <FaCirclePlay
-          className="h-9 w-9 transition-all	hover:scale-110"
+        <div
+          className="h-full p-2 rounded-full bg-white cursor-pointer transition-all	hover:scale-105"
           onClick={onTogglePlay}
-          role="button"
-        />
+        >
+          <svg
+            data-encore-id="icon"
+            role="button"
+            aria-hidden="true"
+            viewBox="0 0 16 16"
+            className="h-4 w-4"
+          >
+            <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
+          </svg>
+        </div>
       )}
       <IoPlaySkipForward
         className="h-6 w-6 transition-all	hover:scale-110"
