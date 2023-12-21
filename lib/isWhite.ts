bc65c61dccfcc12ab06f74fd14cf3663c56adcb6
Hattou Faïssal <faissal.hattou@gmail.com>
2023-12-21 17:31:03 +0100

    :recycle: isWhite() works better

--- lib/isWhite.ts
+++ lib/isWhite.ts
@@ -1,14 +1,12 @@
 import { Color } from "colorthief";
 
-// https://stackoverflow.com/a/9780689
 function isWhite(color: Color): boolean {
   const [R, G, B] = color;
-  const Y = 0.2126 * R + 0.7152 * G + 0.0722 * B;
 
-  // if Y > 128 => white color
-  // if Y < 128 => black color
+  // if R, G, B > 128 => white color
+  // if R, G, B < 128 => black color
 
-  return Y > 128;
+  return R > 200 && G > 200 && B > 200;
 }
 
 export default isWhite;
