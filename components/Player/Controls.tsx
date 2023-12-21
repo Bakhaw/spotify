41b50e93eda0b95d3166385a03fbf6ac2065b53f
Faïssal <fhattou.ext@unowhy.com>
2023-12-21 04:35:15 +0100

    :sparkles: new backward/forward icons

--- components/Player/Controls.tsx
+++ components/Player/Controls.tsx
@@ -1,13 +1,16 @@
-import { FaCirclePlay, FaCirclePause } from "react-icons/fa6";
-import { IoPlaySkipBack, IoPlaySkipForward } from "react-icons/io5";
+import classNames from "classnames";
 
 import { usePlayerContext } from "@/context/PlayerContext";
+import isWhite from "@/lib/isWhite";
 
+import useDominantColor from "@/hooks/useDominantColor";
 import useSpotify from "@/hooks/useSpotify";
+import useTrack from "@/hooks/useTrack";
 
 const Controls = () => {
   const { currentPlaybackState, hydratePlaybackState } = usePlayerContext();
   const spotifyApi = useSpotify();
+  const currentTrack = useTrack(currentPlaybackState?.item?.id);
 
   async function onBackwardButtonClick() {
     await spotifyApi.skipToPrevious();
@@ -42,14 +45,29 @@ const Controls = () => {
       }, 500);
     }
   }
+  const color = useDominantColor(currentTrack?.album.images[0].url);
+
+  const isWhiteBg = isWhite(color);
 
   return (
     <div className="flex justify-center items-center gap-4 h-full w-full">
-      <IoPlaySkipBack
-        className="h-6 w-6 transition-all	hover:scale-110"
-        role="button"
+      <div
+        className="h-full p-2 cursor-pointer transition-all	hover:scale-105"
         onClick={onBackwardButtonClick}
-      />
+      >
+        <svg
+          data-encore-id="icon"
+          role="button"
+          aria-hidden="true"
+          viewBox="0 0 16 16"
+          className={classNames(
+            "h-4 w-4",
+            isWhiteBg ? "fill-black" : "fill-white"
+          )}
+        >
+          <path d="M3.3 1a.7.7 0 0 1 .7.7v5.15l9.95-5.744a.7.7 0 0 1 1.05.606v12.575a.7.7 0 0 1-1.05.607L4 9.149V14.3a.7.7 0 0 1-.7.7H1.7a.7.7 0 0 1-.7-.7V1.7a.7.7 0 0 1 .7-.7h1.6z"></path>
+        </svg>
+      </div>
       {currentPlaybackState?.is_playing ? (
         <div
           className="h-full p-2 rounded-full bg-white cursor-pointer transition-all hover:scale-105"
@@ -81,11 +99,23 @@ const Controls = () => {
           </svg>
         </div>
       )}
-      <IoPlaySkipForward
-        className="h-6 w-6 transition-all	hover:scale-110"
-        role="button"
+      <div
+        className="h-full p-2 cursor-pointer transition-all	hover:scale-105"
         onClick={onForwardButtonClick}
-      />
+      >
+        <svg
+          data-encore-id="icon"
+          role="button"
+          aria-hidden="true"
+          viewBox="0 0 16 16"
+          className={classNames(
+            "h-4 w-4",
+            isWhiteBg ? "fill-black" : "fill-white"
+          )}
+        >
+          <path d="M12.7 1a.7.7 0 0 0-.7.7v5.15L2.05 1.107A.7.7 0 0 0 1 1.712v12.575a.7.7 0 0 0 1.05.607L12 9.149V14.3a.7.7 0 0 0 .7.7h1.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-1.6z"></path>
+        </svg>
+      </div>
     </div>
   );
 };
