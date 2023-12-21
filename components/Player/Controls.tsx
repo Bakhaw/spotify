2bad7f20d8f2468d4352f45203b0a9f681f278ea
Hattou Faïssal <faissal.hattou@gmail.com>
2023-12-21 13:54:08 +0100

    :bug: fix tailwind and controls icons

--- components/Player/Controls.tsx
+++ components/Player/Controls.tsx
@@ -78,7 +78,7 @@ const Controls = () => {
             role="button"
             aria-hidden="true"
             viewBox="0 0 16 16"
-            className="h-4 w-4"
+            className="h-4 w-4 fill-black"
           >
             <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
           </svg>
@@ -93,7 +93,7 @@ const Controls = () => {
             role="button"
             aria-hidden="true"
             viewBox="0 0 16 16"
-            className="h-4 w-4"
+            className="h-4 w-4 fill-black"
           >
             <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
           </svg>
