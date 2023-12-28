01ee9433148fde499f8f7b1511cf3628e6fccfab
Hattou Faïssal <faissal.hattou@gmail.com>
2023-12-28 23:00:48 +0100

    :sparkles: add refetch in TimerStore

--- store/useTimerStore.ts
+++ store/useTimerStore.ts
@@ -3,9 +3,13 @@ import { create } from "zustand";
 interface TimerStore {
   progressMs: number;
   setProgressMs: (progressMs: number) => void;
+  refetch: boolean; // used to know if the current playing track is approaching the end, so we need to update the currentPlaybackState with the next track
+  setRefetch: (refetch: boolean) => void;
 }
 
 export const useTimerStore = create<TimerStore>()((set) => ({
   progressMs: 0,
   setProgressMs: (progressMs) => set({ progressMs }),
+  refetch: false,
+  setRefetch: (refetch) => set({ refetch }),
 }));
