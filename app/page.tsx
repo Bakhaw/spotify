08c21f73b019c36da3eb35d0d734d54532a5881d
Teczer <mehdi.hattou1@gmail.com>
2024-01-07 20:53:50 +0100

    :sparkles :lipstick: :hammer: BlurBackground component : Adding blurBackground to search page

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,12 @@
-"use client";
-
-import Image, { StaticImageData } from "next/image";
-
+import BlurBackground from "@/components/BlurBackground";
 import Container from "@/components/Container";
 import TopArtists from "@/components/TopArtists";
 import TopTracks from "@/components/TopTracks";
 
-import fb1 from "@/assets/FB1.webp";
-import fb2 from "@/assets/FB2.jpg";
-import fb3 from "@/assets/FB3.jpg";
-import fb4 from "@/assets/FB4.jpg";
-
 const Home = () => {
-  function getRandomImage(images: StaticImageData[]): StaticImageData {
-    const randomIndex = Math.floor(Math.random() * images.length);
-
-    return images[randomIndex];
-  }
-
-  const images = [fb1, fb2, fb3, fb4];
-  const image = getRandomImage(images);
-
   return (
     <Container>
-      <Image
-        className="absolute blur-3xl w-full max-h-[300px] z-[-2] object-cover top-0 right-0 opacity-90"
-        alt="blur-background"
-        src={image.src}
-        height={300}
-        width={300}
-        priority={true}
-      />
+      <BlurBackground />
 
       <div className="space-y-8">
         <TopArtists />
