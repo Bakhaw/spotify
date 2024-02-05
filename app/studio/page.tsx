6a4eab79a29ea567b653e7633f27de07c0606181
Teczer <mehdi.hattou1@gmail.com>
2024-02-05 22:46:29 +0100

    :sparkles: Feature : Full Screen mode for Studio

--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import { NextPage } from "next";
 import { LightbulbIcon, LightbulbOffIcon } from "lucide-react";
+import { SlSizeFullscreen } from "react-icons/sl";
+import { MdCloseFullscreen } from "react-icons/md";
 
 import { usePlayerStore } from "@/store/usePlayerStore";
 
@@ -16,12 +18,27 @@ import { Button } from "@/components/ui/button";
 const Studio: NextPage = () => {
   const currentPlaybackState = usePlayerStore((s) => s.currentPlaybackState);
   const [useAlbumColor, setUseAlbumColor] = useState(true);
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
 
   const track = useTrack(currentPlaybackState?.item?.id);
   const dominantColor = useDominantColor(track?.album.images[0].url);
   const backgroundColor = useAlbumColor
     ? generateRGBString(dominantColor)
     : "#000";
+
+  const handleFullscreenToggle = () => {
+    const element = document.documentElement;
+
+    const requestFullscreenMethod = element.requestFullscreen;
+
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+      setIsFullScreen(false);
+    } else if (requestFullscreenMethod) {
+      requestFullscreenMethod.call(element);
+      setIsFullScreen(true);
+    }
+  };
 
   return (
     <div
@@ -31,12 +48,21 @@ const Studio: NextPage = () => {
       }}
     >
       <Button
-        className="z-50 border absolute top-4 right-8"
+        className="z-50 border absolute top-4 right-8 transition-all hover:scale-110"
         onClick={() => setUseAlbumColor((useAlbumColor) => !useAlbumColor)}
         size="icon"
         variant="ghost"
       >
         {useAlbumColor ? <LightbulbIcon /> : <LightbulbOffIcon />}
+      </Button>
+      {/* // FULL SCREEN */}
+      <Button
+        className="z-50 border absolute top-4 right-20 transition-all hover:scale-110"
+        onClick={handleFullscreenToggle}
+        size="icon"
+        variant="ghost"
+      >
+        {isFullScreen ? <MdCloseFullscreen /> : <SlSizeFullscreen />}
       </Button>
 
       <Vinyl />
